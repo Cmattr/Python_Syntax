a603199bde9734994a039bad1e2c6f1cb6209707
Cmattr <cmattr@outlook.com>
2024-03-10 15:38:50 -0400

    Python Syntax Submission

---
+++ PythonComments&Multi-linePractice.py
@@ -0,0 +1,30 @@
+# Task 1:
+
+# python, Oh python, So Clear and so neat
+# With every new challange, Your so hard to beat
+# everything you do makes using you such a treat
+# I write code with you wile sitting in my seat
+# I cant think of another line and i'd hate to repeat
+
+# Task 2
+
+print('''
+Python, in the realm of code your shine, 
+With Simplicity and grace, youre truly divine
+when im lost I look in your documentation for a sign
+When I code using you it always turns out fine
+Come to think of it not all of this  rymed 
+''')
+
+# Task 3
+
+
+# This is from my first poem
+# everything you do makes using you such a treat
+
+#This is from the second
+'''
+when im lost I look in your documentation for a sign
+'''
+x=1
+print(x)
